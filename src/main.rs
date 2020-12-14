81ac93c959ce7868e65d3d0aacea1a1ff2eb9470
Gkn <gokhankabar@hotmail.fr>
2020-12-14 11:07:07 +0100

    RstQcM

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+extern crate gtk;
+mod module;
+
+fn main() {
+    if gtk::init().is_err() {
+        println!("Erreur de l'affichage du logiciel RstQcM");
+        return;
+    }
+    module::qcm();
+}
